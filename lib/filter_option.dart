b74d0db28148ad46fd0b842d9161c12c34fb76e9
Vyshnav <puthen1977@gmail.com>
2024-01-30 16:57:58 +0530

    feature: New Modal Screen

--- lib/filter_option.dart
+++ lib/filter_option.dart
@@ -1,30 +1,85 @@
 import 'package:flutter/material.dart';
 
-class FilterOption {
-  static Future<void> showFilterOptions(BuildContext context) async {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      builder: (context) {
-        return FractionallySizedBox(
-          heightFactor: 0.88,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  "Filters",
-                  style: Theme.of(context).textTheme.displayMedium,
+showFilterBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    builder: (context) {
+      return FractionallySizedBox(
+        heightFactor: 0.88,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                "Filters",
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      // Replace with your DateTimeFormField widget
+                      // For simplicity, it's just a Container
+                      height: 50,
+                      color: Colors.grey[200],
+                    ),
+                  ),
+                ),
+                DropdownMenu<Currency>(
+                  // Replace with your DropdownMenu widget
+                  // For simplicity, it's just a Container
+                  width: 110,
+                ),
+                const SizedBox(width: 16),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, bottom: 5),
+              child: Text(
+                "Portfolio",
+                style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                // Replace with your ChipList or CircularProgressIndicator widget
+                // For simplicity, it's just a Container
+                color: Colors.grey[200],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Remove functionalities
+                },
+                child: const Text("APPLY"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  textStyle: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-              // ... Rest of the code for filter options
-            ],
-          ),
-        );
-      },
-      showDragHandle: true,
-    );
-  }
+            ),
+          ],
+        ),
+      );
+    },
+    showDragHandle: true,
+  );
 }
